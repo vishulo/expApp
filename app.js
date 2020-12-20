e14ce9fc9f92ff7eec7dea56be75eee87a54458f
pravish <lohit.developer@gmail.com>
2020-12-20 17:38:08 +0530

    added routes folder

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const app = express();
 
+// bring routes
 
-app.get("/", (req, res) => {
-    res.send("hello node JS");
-});
+const {getPosts} = require('./routes/post');
 
+app.get("/",getPosts);
 const port = 3000;
+
 app.listen(port, () => {console.log(`A Node JS API is listning on port: ${port}`)});
---
+++ routes/post.js
@@ -0,0 +1,5 @@
+// define the post routes here
+
+exports.getPosts = (req, res) => {
+    res.send("hello node JS ");
+};

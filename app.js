c0121c0f3893893777e65e234b7dd11d9082391e
pravish <lohit.developer@gmail.com>
2020-12-21 18:18:15 +0530

    Added database mongodb

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+//conect to db
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("DB Connected!"));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true, useUnifiedTopology: true });
+// const connection = mongoose.connection;
+// connection.on('open', () => {
+//     console.log('Connected Database Successfully');
+// });
+
 
 // bring routes
-const {getPosts} = require('./routes/post');
-
+//const {getPosts} = require('./routes/post');
+const postRoutes = require('./routes/post');
 // Middleware
 app.use(morgan('dev'));
 // const myMiddlware = (req, res, next) => {
@@ -14,7 +35,9 @@ app.use(morgan('dev'));
 // app.use(myMiddlware);
 
 
-app.get("/",getPosts);
-const port = 3000;
+//app.get("/",getPosts);
+
+app.use("/", postRoutes);
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {console.log(`A Node JS API is listning on port: ${port}`)});
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,12 @@
 // define the post routes here
 
-exports.getPosts = (req, res) => {
-    res.send("hello node JS ");
-};
+const express = require('express');
+
+//bring in controller
+const postController = require("../controllers/post");
+
+const router = express.Router();
+
+router.get("/",  postController.getPosts);
+
+module.exports = router;
